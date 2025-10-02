fd1a1d88ed652faa458a816a96ba2774dd3578bb
raihanislam-71 <raihanislam778571@gmail.com>
2025-10-03 00:51:40 +0600

    first commit

---
+++ Algorithm/Module_12/matrix._to_edge_list.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Edge
+{
+    public:
+    int u,v,w;
+    Edge(int u,int v,int w)
+    {
+        this-> u = u;
+        this-> v = v;
+        this-> w = w;
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    int mat[n][n];
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cin >> mat[i][j];
+        }
+    }
+    vector<Edge> edgeList;
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(mat[i][j] > 0 && i != j)
+            {
+                edgeList.push_back(Edge(i,j,mat[i][j]));
+            }
+        }
+    }
+
+    for(Edge ed : edgeList)
+    {
+        cout<<ed.u<<" "<<ed.v<<" "<<ed.w<<endl;
+    }
+    return 0;
+}
