04d8c8928990960390d68df8b7f44ac1ee04d00e
raihanislam-71 <raihanislam778571@gmail.com>
2025-10-02 16:56:21 +0600

    first commit

---
+++ Algorithm/Module_12/adj_list_to_matrix_2.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+void convert(int n, vector<pair<int, int>> adj[])
+{
+    int mat[n][n];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                mat[i][j] = 0;
+            else
+                mat[i][j] = -1;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (pair<int, int> child : adj[i])
+        {
+            int childNode = child.first;
+            int cost = child.second;
+            mat[i][childNode] = cost;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<pair<int, int>> v[n];
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        v[a].push_back({b, c});
+    }
+    convert(n, v);
+    return 0;
+}
--- Algorithm/Module_4/assignment_1/Connected_or_Not.cpp
+++ Algorithm/Module_4/assignment_1/Connected_or_Not.cpp
@@ -5,7 +5,22 @@ int main()
     int n, e;
     cin >> n >> e;
     int m[n][n];
-    memset(m,0,sizeof(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                m[i][j] = 0;
+            }
+
+            else
+            {
+                m[i][j] = -1;
+            }
+        }
+    }
+
     while (e--)
     {
         int a, b;
@@ -18,7 +33,11 @@ int main()
     {
         int x, y;
         cin >> x >> y;
-        if (m[x][y] != 1)
+        if (m[x][y] == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else if (m[x][y] == 0)
         {
             cout << "YES" << endl;
         }
